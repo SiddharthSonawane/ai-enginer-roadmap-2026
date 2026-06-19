ae689daeabc2885501c6a04c99a592bf8d2dfaac
SiddharthSonawane <sidharthsonavane@gmail.com>
2026-06-19 11:09:46 +0530

    functions

---
+++ python-fundamentals/functions/main.py
@@ -0,0 +1,8 @@
+def evenOdd(x):
+    if (x % 2 == 0):
+        return str(x) + " is Even number"
+    else:
+        return str(x) + " is Odd number"
+
+print(evenOdd(4))
+print(evenOdd(13))
---
+++ python-fundamentals/loops/main.py
@@ -0,0 +1,12 @@
+# types of loops in Python
+# 1. for loop
+# 2. while loop
+
+# for loop
+numbers = [1, 2, 3, 4, 5]
+for number in numbers:
+    print(number)
+
+# loop with range
+for i in range(5):
+    print(i)
